498032116048280e5cf7b089486769520ec44d53
Helena Geronimo <HELENA@Helenas-MacBook-Pro.local>
2022-04-25 23:12:40 +0800

    first commit

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Events\WebsocketDemoEvent;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    broadcast(new WebsocketDemoEvent('some data'));
+
+    return view('welcome');
+});
+
+Route::get('/chats', 'App\Http\Controllers\ChatsController@index');
+
+Route::get('/messages', 'App\Http\Controllers\ChatsController@fetchMessages');
+Route::post('/messages', 'App\Http\Controllers\ChatsController@sendMessage');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
